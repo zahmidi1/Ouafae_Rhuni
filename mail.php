8b710b036f0ff11adb8a71787f8bb2b5663bcbfd
ayoub-maskoul <maskoul.ayoub@ofppt-edu.ma>
2025-01-31 11:07:54 +0100

    fix email

--- mail.php
+++ mail.php
@@ -1,52 +1,62 @@
 <?php
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+$message = "";
 $method = $_SERVER['REQUEST_METHOD'];
-
-//Script Foreach
 $c = true;
-if ( $method === 'POST' ) {
 
-	$project_name = trim($_POST["project_name"]);
-	$admin_email  = trim($_POST["admin_email"]);
-	$form_subject = trim($_POST["form_subject"]);
+try {
+	if ($method === 'POST' || $method === 'GET') {
+		$project_name = trim($_REQUEST["project_name"]);
+		$admin_email  = trim($_REQUEST["admin_email"]);
+		$form_subject = trim($_REQUEST["form_subject"]);
 
-	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
+		foreach ($_REQUEST as $key => $value) {
+			if ($value != "" && !in_array($key, ["project_name", "admin_email", "form_subject"])) {
+				$message .= "
+                " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+                    <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>" . htmlspecialchars($key) . "</b></td>
+                    <td style='padding: 10px; border: #e9e9e9 1px solid;'>" . htmlspecialchars($value) . "</td>
+                </tr>
+                ";
+			}
 		}
 	}
-} else if ( $method === 'GET' ) {
 
-	$project_name = trim($_GET["project_name"]);
-	$admin_email  = trim($_GET["admin_email"]);
-	$form_subject = trim($_GET["form_subject"]);
+	$message = "<table style='width: 100%;'>$message</table>";
 
-	foreach ( $_GET as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
+	$mail = new PHPMailer(true);
+
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+	$mail->isSMTP();
+	$mail->Host       = 'smtp.gmail.com';
+	$mail->SMTPAuth   = true;
+	$mail->Username   = 'maskoul1ayoub@gmail.com
+';                 //SMTP username (your Gmail address)
+	$mail->Password   = 'pygsgodidqsvwfsk';
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+	$mail->Port       = 587;
+
+	//Recipients
+	$mail->setFrom($admin_email, $project_name);
+	$mail->addAddress('maskoul1ayoub@gmail.com'); //rhuniouafae@gmail.com                       
+	$mail->addReplyTo('maskoul1ayoub@gmail.com'); //rhuniouafae@gmail.com
+
+	//Content
+	$mail->isHTML(true);
+	$mail->Subject = $form_subject;
+	$mail->Body    = $message;
+	$mail->AltBody = strip_tags($message);
+
+	$mail->send();
+	echo 'Message has been sent successfully';
+} catch (Exception $e) {
+	error_log("Mail Error: {$mail->ErrorInfo}");
+	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-
-$message = "<table style='width: 100%;'>$message</table>";
-
-function adopt($text) {
-	return '=?UTF-8?B?'.Base64_encode($text).'?=';
-}
-
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
-'Reply-To: '.$admin_email.'' . PHP_EOL;
-
-mail($admin_email, adopt($form_subject), $message, $headers );
